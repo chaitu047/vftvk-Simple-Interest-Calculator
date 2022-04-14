d9ff8dfef48c4ad95066bc12b92ef7cf99594d65
chaitu047 <chaitu.klk047@gmail.com>
2022-04-14 16:23:22 +0530

    completed full stack project

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    //fetch principal amount
+    var principal = document.getElementById("principal").value;
+    var res_visible = true;
+    //validate principal amount
+    if (principal == '' || principal == ' ' || principal <= 0 || isNaN(principal)) {
+        alert('Enter a positive number');
+        document.getElementById("principal").focus();
+        res_visible = false;
+    }
+    //show the result only if principal amount is valid
+    if (res_visible) {
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+        var year = new Date().getFullYear() + parseInt(years);
+        var totalAmount = interest + principal;
+        document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,<br/>at an interest rate of <mark>${rate}</mark>%.<br/>You will receive an amount of <mark>${totalAmount}</mark>,<br/>in the year <mark>${year}</mark>`;
+    }
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
